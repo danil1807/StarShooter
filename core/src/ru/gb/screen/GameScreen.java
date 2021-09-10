8476a694784f4fb8b2a7dade47dfdc93743f7aa1
Danil <danil.klimentev@gmail.com>
2021-09-10 08:59:44 +0500

    1 - self-shooting with option of increasing speed due to level 2 - adding horrible bullet sound to shoot 3 - adding music to the menu game

---
+++ core/src/ru/gb/screen/GameScreen.java
@@ -0,0 +1,114 @@
+package ru.gb.screen;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.gb.base.BaseScreen;
+import ru.gb.math.Rect;
+import ru.gb.pool.BulletPool;
+import ru.gb.sprite.Background;
+import ru.gb.sprite.Star;
+import ru.gb.sprite.StarShip;
+
+public class GameScreen extends BaseScreen {
+
+    private static final int STAR_COUNT = 64;
+
+    private Texture bg;
+    private TextureAtlas atlas;
+
+    private Background background;
+    private Star[] stars;
+    private StarShip mainShip;
+
+    private BulletPool bulletPool;
+
+    @Override
+    public void show() {
+        super.show();
+        bg = new Texture("textures/bg.png");
+        atlas = new TextureAtlas("textures/mainAtlas.tpack");
+
+        background = new Background(bg);
+        stars = new Star[STAR_COUNT];
+        for (int i = 0; i < stars.length; i++) {
+            stars[i] = new Star(atlas);
+        }
+        bulletPool = new BulletPool();
+        mainShip = new StarShip(atlas, bulletPool);
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        update(delta);
+        freeAllDestroyed();
+        draw();
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
+        for (Star star : stars) {
+            star.resize(worldBounds);
+        }
+        mainShip.resize(worldBounds);
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        bg.dispose();
+        atlas.dispose();
+        bulletPool.dispose();
+    }
+
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
+        mainShip.touchDown(touch, pointer, button);
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(Vector2 touch, int pointer, int button) {
+        mainShip.touchUp(touch, pointer, button);
+        return false;
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        mainShip.keyDown(keycode);
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        mainShip.keyUp(keycode);
+        return false;
+    }
+
+    private void update(float delta) {
+        for (Star star : stars) {
+            star.update(delta);
+        }
+        mainShip.update(delta);
+        bulletPool.updateActiveSprites(delta);
+    }
+
+    private void freeAllDestroyed() {
+        bulletPool.freeAllDestroyedActiveSprites();
+    }
+
+    private void draw() {
+        batch.begin();
+        background.draw(batch);
+        for (Star star : stars) {
+            star.draw(batch);
+        }
+        mainShip.draw(batch);
+        bulletPool.drawActiveSprites(batch);
+        batch.end();
+    }
+}
