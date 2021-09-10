4b542f152488c3d94028ccdced1a03e9a813f159
danil1807 <74928435+danil1807@users.noreply.github.com>
2021-09-10 08:52:28 +0500

    Revert "lesson5hw"

--- core/src/ru/geekbrains/StarGame.java
+++ core/src/ru/geekbrains/StarGame.java
@@ -1,14 +1,34 @@
-package ru.gb;
+package ru.geekbrains;
 
-import com.badlogic.gdx.Game;
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
 
-import ru.gb.screen.MenuScreen;
-
-public class StarGame extends Game {
+public class StarGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	Texture background;
 	
 	@Override
 	public void create () {
-		setScreen(new MenuScreen());
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+		background = new Texture("space-table.png");
 	}
 
+	@Override
+	public void render () {
+		ScreenUtils.clear(1, 0, 0, 1);
+		batch.begin();
+		batch.draw(background,0,0);
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+		background.dispose();	}
 }
